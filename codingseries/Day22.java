f21226a3ce770781cb6cde939fbdc11eb13b61a7
Vishwanath1711 <vishwanathkustagi308@gmail.com>
2023-03-26 18:50:59 +0530

    Solution for Day22 Problem

--- codingseries/Day22.java
+++ codingseries/Day22.java
@@ -1,7 +1,14 @@
 package codingseries;
-
+/*
+Day 22 coding Statement : Write a program to express a number as a sum of two prime numbers
+Description
+Get a number as input from the user and express that number as sum of two prime numbers.
+Input
+4
+Output
+4 can be expressed as sum of 2 and 2
+ */
 import java.util.Scanner;
-
 public class Day22 {
 
     static int sumprimes(int n)
