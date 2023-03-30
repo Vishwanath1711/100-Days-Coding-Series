03e622faa866e1add1c8e9ceb5fb539f6afca427
Vishwanath1711 <vishwanathkustagi308@gmail.com>
2023-03-30 17:02:53 +0530

    Solution for Day26 Problem

---
+++ codingseries/Day26.java
@@ -0,0 +1,30 @@
+package codingseries;
+/*
+Day 26 coding Statement : Write a program to calculate Maximum number of handshakes
+Description
+Get the number of people in the room as input from the user.
+Then calculate the maximum number of handshakes possible among that people.
+For e.g. If there are N people in the room then the first person has to shake hand with N-1 people
+and second person has to shake hand with N-1-1 people i.e., N-2 handshakes are possible. Thus, it goes on.
+So total hand shakes = N-1 + N-2 + N-3 +………+1 + 0
+Input
+10
+Output
+45
+ */
+import java.util.Scanner;
+public class Day26 {
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the number of people in room:");
+        int n = S.nextInt();
+        int noOfHandshakes = n*(n-1)/2;
+        /*
+        for(int i=1; i<=n;i++){
+            noOfHandshakes+=n-i;
+        }
+        */
+        System.out.println(noOfHandshakes);
+    }
+}
