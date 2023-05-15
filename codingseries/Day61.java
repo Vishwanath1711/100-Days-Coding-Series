d9e040b34372ae4507f5a91bbca35fbad4895482
Vishwanath1711 <vishwanathkustagi308@gmail.com>
2023-05-15 17:16:59 +0530

    Solution for Day61 Problem

---
+++ codingseries/Day61.java
@@ -0,0 +1,47 @@
+package codingseries;
+/*
+Day 61 coding Statement : Chess Format
+Given the time control of a chess match as a+b, determine which format of chess out of the given 4 it belongs to.
+1) Bullet if a+b<3
+2) Blitz if 3≤a+b≤10
+3) Rapid if 11≤a+b≤60
+4) Classical if 60<a+b
+Input Format
+First line will contain T, number of testcases. Then the testcases follow.
+Each testcase contains a single line of input, two integers a,b
+Output Format
+For each testcase, output in a single line, answer 1 for bullet, 2 for blitz, 3 for rapid, and 4 for classical format.
+Sample Input 1
+4
+1 0
+4 1
+100 0
+20 5
+Sample Output 1
+1
+2
+4
+3
+ */
+import java.util.Scanner;
+public class Day61 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("enter the number of test cases");
+        int t = S.nextInt();
+        for (int i=0;i<t;i++){
+            int a = S.nextInt();
+            int b = S.nextInt();
+            int sum;
+            sum=a+b;
+            if (sum<3)
+                System.out.println("1");
+            else if (sum<=10)
+                System.out.println("2");
+            else if (sum<=60)
+                System.out.println("3");
+            else
+                System.out.println("4");
+        }
+    }
+}
